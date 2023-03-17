4a7185dc45989d34ad6dedf851265b034aa197f6
Mackay Zhou <Mackay Zhou>
2023-03-17 16:03:16 +0800

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5MTAyMQ/spectral.js?branch_slug=main&token=04637bb2-329c-4a3f-b26d-b41d1ddccb84";
+export default { extends: ruleset };
